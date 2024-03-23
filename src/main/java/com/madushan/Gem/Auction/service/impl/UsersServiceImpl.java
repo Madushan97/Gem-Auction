24cdcb263d5776fb726bf4451c5fcb77eaf6334f
madushan <madushannipuna1997@gmail.com>
2024-03-24 01:24:49 +0530

    logging into user

--- src/main/java/com/madushan/Gem/Auction/service/impl/UsersServiceImpl.java
+++ src/main/java/com/madushan/Gem/Auction/service/impl/UsersServiceImpl.java
@@ -1,11 +1,14 @@
 package com.madushan.Gem.Auction.service.impl;
 
+import com.madushan.Gem.Auction.controller.UserController;
 import com.madushan.Gem.Auction.dto.requestDto.UserRequestDto;
 import com.madushan.Gem.Auction.dto.responseDto.UserResponseDto;
 import com.madushan.Gem.Auction.model.User;
 import com.madushan.Gem.Auction.repository.UserRepository;
 import com.madushan.Gem.Auction.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,26 +19,33 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UsersServiceImpl implements UserService {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(UsersServiceImpl.class);
+
     private final UserRepository userRepository;
 
     @Override
     public List<UserResponseDto> getAllUsers() {
         List<User> userList = userRepository.findAllByActiveStatus(true);
-
         List<UserResponseDto> userResponseDtoList = new ArrayList<>();
-        for (User user : userList) {
-            UserResponseDto userResponseDto = new UserResponseDto();
-            userResponseDto.setId(user.getId());
-            userResponseDto.setUsername(user.getUsername());
-            userResponseDto.setEmail(user.getEmail());
-            userResponseDto.setPassword(user.getPassword());
-            userResponseDto.setAddress(user.getAddress());
-            userResponseDto.setPhoneNumber(user.getPhoneNumber());
-            userResponseDto.setActiveStatus(user.getActiveStatus());
-            userResponseDto.setAuction(user.getAuction());
-            userResponseDto.setCreatedAt(user.getCreatedAt());
-            userResponseDto.setUpdatedAt(user.getUpdatedAt());
-            userResponseDtoList.add(userResponseDto);
+        if (userList.size() > 0) {
+            LOGGER.info("Active users getting successfully, user List size : {}", userList.size());
+            for (User user : userList) {
+                UserResponseDto userResponseDto = new UserResponseDto();
+                userResponseDto.setId(user.getId());
+                userResponseDto.setUsername(user.getUsername());
+                userResponseDto.setEmail(user.getEmail());
+                userResponseDto.setPassword(user.getPassword());
+                userResponseDto.setAddress(user.getAddress());
+                userResponseDto.setPhoneNumber(user.getPhoneNumber());
+                userResponseDto.setActiveStatus(user.getActiveStatus());
+                userResponseDto.setAuction(user.getAuction());
+                userResponseDto.setCreatedAt(user.getCreatedAt());
+                userResponseDto.setUpdatedAt(user.getUpdatedAt());
+                userResponseDtoList.add(userResponseDto);
+            }
+            LOGGER.info("User list sent successfully");
+        } else {
+            LOGGER.warn("User list is empty");
         }
         return userResponseDtoList;
     }
@@ -56,15 +66,19 @@ public class UsersServiceImpl implements UserService {
         user.setUpdatedAt(new Date());
 
         userRepository.save(user);
+        LOGGER.info("User create successfully");
         return "User ID : "+ user.getId() + ", saved.";
     }
 
     @Override
     public String deleteUser(int userId) {
         User user = userRepository.findByIdAndActiveStatus(userId, true);
+        LOGGER.info("User ID : {} fetch successfully", userId);
         if (user != null) {
             userRepository.deleteById(userId);
+            LOGGER.info("User ID : {} delete successfully", userId);
         }
+        LOGGER.error("User ID : {} is null", userId);
         return "Delete ID : " + userId + ", successfully";
     }
 }
